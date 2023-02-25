37381d67971a9b3610928f0788868cd126b9f7c3
PauloGN <paulorsantos.gnt@outlook.com>
2023-02-24 17:19:48 -0800

    29min
    calculate total force function

--- REngine/Framework/AI/Inc/SteeringModule.h
+++ REngine/Framework/AI/Inc/SteeringModule.h
@@ -4,15 +4,26 @@
 
 namespace AI
 {
-
 	class SteeringModule final
 	{
 	public:
 
-		//~SteeringModule();
+		SteeringModule(Agent& agent);
+
+		template<class NewBehaviorType>
+		NewBehaviorType* AddBehavior()
+		{
+			static_assert(std::is_base_of_v<SteeringBehavior, NewBehaviorType>, "Only Behavior derived from Steering behavior are allowed.");
+
+			auto& newBehavior = mBehaviors.emplace_back(std::make_unique<NewBehaviorType>());
+
+			return static_cast<NewBehaviorType*>(newBehavior.get());
+		}
+
+		std::pair<float, float>Calculate();
 
 	private:
-
+		Agent& mAgent;
+		std::vector<std::unique_ptr<SteeringBehavior>> mBehaviors;
 	};
-
 }
--- REngine/Framework/AI/Src/SteeringModule.cpp
+++ REngine/Framework/AI/Src/SteeringModule.cpp
@@ -1,3 +1,21 @@
 #include "Precompiled.h"
 #include "SteeringModule.h"
 
+AI::SteeringModule::SteeringModule(Agent& agent)
+	:mAgent(agent)
+{
+}
+
+std::pair<float, float> AI::SteeringModule::Calculate()
+{
+	std::pair<float, float> totalForce;
+
+	for (auto& behavior: mBehaviors)
+	{
+		totalForce.first += behavior->Calculate(mAgent).first;
+		totalForce.second += behavior->Calculate(mAgent).second;
+
+	}
+
+	return totalForce;
+}
